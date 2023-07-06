e342032ac6b8dfd49d969c6bb6ca9c6facb0f9f8
Jacek Gizinski <jacektrg+github@gmail.com>
2023-07-06 23:02:51 +0200

    Adding default data for forms.

---
+++ app/lib/defaults.ts
@@ -0,0 +1,35 @@
+import {
+	BusinessAccFormData,
+	LoginData,
+	RegularAccFormData,
+	VIPAccFormData,
+} from './types';
+
+export const regularAccFormData: RegularAccFormData = {
+	first_name: '',
+	last_name: '',
+	email: '',
+	password: '',
+};
+
+export const vipAccFormData: VIPAccFormData = {
+	first_name: '',
+	last_name: '',
+	email: '',
+	password: '',
+	extra_funds: '10000',
+};
+
+export const businessAccFormData: BusinessAccFormData = {
+	first_name: '',
+	last_name: '',
+	company_name: '',
+	address: '',
+	email: '',
+	password: '',
+};
+
+export const loginData: LoginData = {
+	account: '',
+	password: '',
+};
