c72632493eb0095c1830871da84889239c87d41c
Jacek Gizinski <jacektrg+github@gmail.com>
2023-07-06 23:01:38 +0200

    Changing div tag to button.
    Adding support for optional cta function and style class names.

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -2,21 +2,29 @@ import Link from 'next/link';
 
 export default function Button({
 	children,
+	styleClass,
 	href,
+	cta,
 }: {
 	children?: React.ReactNode;
+	styleClass?: string;
 	href?: string;
+	cta?: (any?: any) => void;
 }) {
 	return href ? (
 		<Link
 			href={href}
-			className='py-1 px-2 rounded-md font-bold text-yellow-300 bg-purple-700'
+			className={`py-1 px-2 rounded-md font-bold text-yellow-300 bg-purple-700 whitespace-nowrap ${styleClass}`}
 		>
 			{children || 'Button'}
 		</Link>
 	) : (
-		<div className='py-1 px-2 rounded-md font-bold text-yellow-300 bg-purple-700'>
+		<button
+			type='button'
+			className={`py-1 px-2 rounded-md font-bold text-yellow-300 bg-purple-700 whitespace-nowrap ${styleClass}`}
+			onClick={() => (cta ? cta() : null)}
+		>
 			{children || 'Button'}
-		</div>
+		</button>
 	);
 }
