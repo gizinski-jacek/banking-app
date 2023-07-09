e00000680c6672ae5252921bc3047dd6b444ec16
Jacek Gizinski <jacektrg+github@gmail.com>
2023-07-10 00:39:29 +0200

    Adding yup and schemas
    for checking form fields.

---
+++ app/lib/yup.ts
@@ -0,0 +1,91 @@
+import * as Yup from 'yup';
+import {
+	BusinessAccFormData,
+	RegularAccFormData,
+	VIPAccFormData,
+} from './types';
+
+export default async function yupValidation<T = Record<string, any>>(
+	scheme: Yup.Schema<T>,
+	data: Record<string, any> | null
+) {
+	try {
+		await scheme.validate(data, { abortEarly: false });
+		return { isValid: true, errors: null };
+	} catch (error: any) {
+		const { errors } = error;
+		return { isValid: false, errors };
+	}
+}
+
+export const regularAccFormValidationSchema: Yup.Schema<RegularAccFormData> =
+	Yup.object().shape({
+		first_name: Yup.string()
+			.required('First name is required.')
+			.min(4, 'First name min 4 characters.')
+			.max(16, 'First name max 16 characters.'),
+		last_name: Yup.string()
+			.required('Last name is required.')
+			.min(4, 'Last name min 4 characters.')
+			.max(32, 'Last name max 32 characters.'),
+		email: Yup.string()
+			.required('Email is required.')
+			.min(8, 'Email min 8 characters.')
+			.max(64, 'Email max 64 characters.'),
+		password: Yup.string()
+			.required('Password is required.')
+			.min(8, 'Password min 8 characters.')
+			.max(128, 'Password max 128 characters.'),
+	});
+
+export const vipAccFormValidationSchema: Yup.Schema<VIPAccFormData> =
+	Yup.object().shape({
+		first_name: Yup.string()
+			.required('First name is required.')
+			.min(4, 'First name min 4 characters.')
+			.max(16, 'First name max 16 characters.'),
+		last_name: Yup.string()
+			.required('Last name is required.')
+			.min(4, 'Last name min 4 characters.')
+			.max(32, 'Last name max 32 characters.'),
+		email: Yup.string()
+			.required('Email is required.')
+			.min(8, 'Email min 8 characters.')
+			.max(64, 'Email max 64 characters.'),
+		password: Yup.string()
+			.required('Password is required.')
+			.min(8, 'Password min 8 characters.')
+			.max(128, 'Password max 128 characters.'),
+		extra_funds: Yup.number()
+			.required('Extra funds is required.')
+			.min(0, 'Extra funds min 0 characters.')
+			.max(1000000, 'Extra funds max 1000000 characters.'),
+	});
+
+export const businessAccFormValidationSchema: Yup.Schema<BusinessAccFormData> =
+	Yup.object().shape({
+		first_name: Yup.string()
+			.required('First name is required.')
+			.min(4, 'First name min 4 characters.')
+			.max(16, 'First name max 16 characters.'),
+		last_name: Yup.string()
+			.required('Last name is required.')
+			.min(4, 'Last name min 4 characters.')
+			.max(32, 'Last name max 32 characters.'),
+		email: Yup.string()
+			.required('Email is required.')
+			.min(8, 'Email min 8 characters.')
+			.max(64, 'Email max 64 characters.'),
+		password: Yup.string()
+			.required('Password is required.')
+			.min(8, 'Password min 8 characters.')
+			.max(128, 'Password max 128 characters.'),
+		company_name: Yup.string()
+			.required('Company is required.')
+			.min(4, 'Company min 4 characters.')
+			.max(128, 'Company max 128 characters.'),
+		address: Yup.string()
+			.required('Address is required.')
+			.min(4, 'Address min 4 characters.')
+			.max(128, 'Address max 128 characters.'),
+	});
