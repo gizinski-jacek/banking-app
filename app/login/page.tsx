d2f1faafea4131ed2e700882e7af8f4dd104643b
Jacek Gizinski <jacektrg+github@gmail.com>
2023-07-10 00:37:21 +0200

    Styling, grouping form elements.
    Importing custom button.
    Renaming states and functions.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,35 +4,35 @@ import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { LoginData } from '../lib/types';
 import { loginData } from '../lib/defaults';
+import Button from '../components/Button';
 
 export default function Login() {
 	const router = useRouter();
 	const [formData, setFormData] = useState<LoginData>(loginData);
-	const [idValid, setIdValid] = useState<null | boolean | string>(null);
+	const [accountValid, setAccountValid] = useState<null | boolean | string>(
+		null
+	);
 	const [passwordValid, setPasswordValid] = useState<null | boolean | string>(
 		null
 	);
 
-	function handleFormChange(e: React.FormEvent) {
-		const { name, value } = e.target as typeof e.target & {
-			name: string;
-			value: string;
-		};
+	function handleFormChange(e: React.ChangeEvent<HTMLInputElement>) {
+		const { name, value } = e.target;
 		setFormData({ ...formData, [name]: value });
 	}
 
-	async function checkAccount(account: string) {
+	async function verifyAccount(data: LoginData) {
 		try {
 			// API request to check for ID in DB
 			// Toggle to password form on success
-			setIdValid(true);
+			setAccountValid(true);
 		} catch (error) {
 			console.log(error);
-			setIdValid('Incorrect ID');
+			setAccountValid('Incorrect account');
 		}
 	}
 
-	async function checkPassword(password: string) {
+	async function verifyPassword(data: LoginData) {
 		try {
 			// API request to check if password is valid
 			// Redirect to dashboard on success
@@ -45,40 +45,60 @@ export default function Login() {
 
 	return (
 		<div>
-			{!idValid ? (
+			{!accountValid ? (
 				<form>
-					<label htmlFor='account'>Login ID</label>
-					<input
-						type='text'
-						id='account'
-						name='account'
-						value={formData.account}
-						required
-						onChange={handleFormChange}
-					/>
-					<button type='button' onClick={(e) => checkAccount(formData.account)}>
+					<fieldset>
+						<label htmlFor='account'>Account ID</label>
+						<input
+							type='text'
+							id='account'
+							name='account'
+							value={formData.account}
+							required
+							onChange={handleFormChange}
+						/>
+						{typeof accountValid === 'string' && (
+							<p className='text-red-600 font-weight-semibold'>
+								{accountValid}
+							</p>
+						)}
+					</fieldset>
+					<Button
+						styleClass='my-3 col-span-2 mx-auto'
+						cta={() => verifyAccount(formData)}
+					>
 						Submit
-					</button>
+					</Button>
 				</form>
 			) : (
 				<form>
-					<label htmlFor='password'>Password</label>
-					<input
-						type='password'
-						id='password'
-						name='password'
-						value={formData.password}
-						required
-						onChange={handleFormChange}
-					/>
-					<button
-						type='button'
-						onClick={(e) => checkPassword(formData.password)}
+					<fieldset>
+						<label htmlFor='password'>Password</label>
+						<input
+							type='password'
+							id='password'
+							name='password'
+							value={formData.password}
+							required
+							onChange={handleFormChange}
+						/>
+						{typeof passwordValid === 'string' && (
+							<p className='text-red-600 font-weight-semibold'>
+								{passwordValid}
+							</p>
+						)}
+					</fieldset>
+					<Button
+						styleClass='my-3 col-span-2 mx-auto'
+						cta={() => verifyPassword(formData)}
 					>
 						Submit
-					</button>
+					</Button>
 				</form>
 			)}
+			<Button href='/' styleClass='my-3'>
+				{`<-`} Go Back
+			</Button>
 		</div>
 	);
 }
