be6d16f74609cb3ed852463efad4febdb735bdb2
Jacek Gizinski <jacektrg+github@gmail.com>
2023-07-10 23:15:06 +0200

    Adding custom colors for dark and light themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,33 @@ module.exports = {
 	],
 	theme: {
 		extend: {},
+		colors: {
+			'bg-color': {
+				light: 'rgb(235, 220, 150)',
+				DEFAULT: 'rgb(235, 220, 150)',
+				dark: 'rgb(75, 0, 125)',
+			},
+			'text-color': {
+				light: 'rgb(75, 0, 125)',
+				DEFAULT: 'rgb(75, 0, 125)',
+				dark: 'rgb(235, 220, 150)',
+			},
+			'btn-bg-color': {
+				light: 'rgb(75, 0, 125)',
+				DEFAULT: 'rgb(75, 0, 125)',
+				dark: 'rgb(235, 220, 150)',
+			},
+			'btn-text-color': {
+				light: 'rgb(255, 50, 100)',
+				DEFAULT: 'rgb(255, 50, 100)',
+				dark: 'rgb(180, 50, 75)',
+			},
+			'shadow-color': {
+				light: 'rgb(180, 50, 75)',
+				DEFAULT: 'rgb(180, 50, 75)',
+				dark: 'rgb(0, 215, 200)',
+			},
+		},
 	},
 	plugins: [],
 };
