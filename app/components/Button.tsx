9991249d6a819ec80c0134ccd09920eeeb33096f
Jacek Gizinski <jacektrg+github@gmail.com>
2023-07-10 23:22:33 +0200

    Removing tailwind styling classes.

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -12,16 +12,13 @@ export default function Button({
 	cta?: (any?: any) => void;
 }) {
 	return href ? (
-		<Link
-			href={href}
-			className={`py-1 px-2 rounded-md font-bold text-yellow-300 bg-purple-700 whitespace-nowrap ${styleClass}`}
-		>
+		<Link href={href} className={styleClass}>
 			{children || 'Button'}
 		</Link>
 	) : (
 		<button
 			type='button'
-			className={`py-1 px-2 rounded-md font-bold text-yellow-300 bg-purple-700 whitespace-nowrap ${styleClass}`}
+			className={styleClass}
 			onClick={() => (cta ? cta() : null)}
 		>
 			{children || 'Button'}
