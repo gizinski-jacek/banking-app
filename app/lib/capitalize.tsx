e671e898720fe161804687839a50c0f66335217f
Jacek Gizinski <jacektrg+github@gmail.com>
2023-07-10 23:26:19 +0200

    Renaming file

--- app/lib/capitalize.tsx
+++
@@ -1,8 +0,0 @@
-export default function capitalize(string: string): string {
-	const array = string.split(/[-,_, ]/gm);
-	const capitalized = array.map(
-		(string) => string.charAt(0).toUpperCase() + string.slice(1)
-	);
-
-	return capitalized.join(' ');
-}
