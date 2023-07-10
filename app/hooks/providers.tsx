f89da859d10b9a3f6bae68b37017f8572f31b9c9
Jacek Gizinski <jacektrg+github@gmail.com>
2023-07-10 23:23:32 +0200

    Adding hook providing next-themes theme provider.
    Waiting for component to mount before rendering contents to make
    component hydration safe,
    otherwise rendering contents without theme provider.

---
+++ app/hooks/providers.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { ThemeProvider } from 'next-themes';
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+	const [mounted, setMounted] = useState(false);
+
+	useEffect(() => {
+		setMounted(true);
+	}, []);
+
+	if (!mounted) {
+		return <>{children}</>;
+	}
+	return <ThemeProvider>{children}</ThemeProvider>;
+}
