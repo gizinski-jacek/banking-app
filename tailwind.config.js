3a0e6e0264d38a0c1e3376230da2ec5b5498fe86
Jacek Gizinski <jacektrg+github@gmail.com>
2023-07-11 21:41:13 +0200

    Updating colors with css variables.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,33 +6,17 @@ module.exports = {
 		'./app/**/*.{js,ts,jsx,tsx,mdx}',
 	],
 	theme: {
-		extend: {},
+		extend: {
+			transitionProperty: {
+				width: 'width',
+			},
+		},
 		colors: {
-			'bg-color': {
-				light: 'rgb(235, 220, 150)',
-				DEFAULT: 'rgb(235, 220, 150)',
-				dark: 'rgb(75, 0, 125)',
-			},
-			'text-color': {
-				light: 'rgb(75, 0, 125)',
-				DEFAULT: 'rgb(75, 0, 125)',
-				dark: 'rgb(235, 220, 150)',
-			},
-			'btn-bg-color': {
-				light: 'rgb(75, 0, 125)',
-				DEFAULT: 'rgb(75, 0, 125)',
-				dark: 'rgb(235, 220, 150)',
-			},
-			'btn-text-color': {
-				light: 'rgb(255, 50, 100)',
-				DEFAULT: 'rgb(255, 50, 100)',
-				dark: 'rgb(180, 50, 75)',
-			},
-			'shadow-color': {
-				light: 'rgb(180, 50, 75)',
-				DEFAULT: 'rgb(180, 50, 75)',
-				dark: 'rgb(0, 215, 200)',
-			},
+			'cstm-bg': 'var(--cstm-bg)',
+			'cstm-color': 'var(--cstm-color)',
+			'cstm-btn-bg': 'var(--cstm-btn-bg)',
+			'cstm-btn-color': 'var(--cstm-btn-color)',
+			'cstm-shadow': 'var(--cstm-shadow)',
 		},
 	},
 	plugins: [],
