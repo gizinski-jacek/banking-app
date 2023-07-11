2fdbf0fe96036a62050c12ed736a352d17bdc3ec
Jacek Gizinski <jacektrg+github@gmail.com>
2023-07-11 23:52:35 +0200

    Readding tailwindcss style classes.
    Updating props types.
    Adding props used for styling anchor to look
    like button and to animation on hover that fills up tag element.

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,24 +1,57 @@
 import Link from 'next/link';
 
+interface CommonProps {
+	children?: React.ReactNode;
+	styleClass?: string;
+}
+
+type ConditionalProps =
+	| {
+			href?: string;
+			buttonLike?: boolean;
+			fillUp?: never;
+			cta?: never;
+	  }
+	| {
+			href?: string;
+			buttonLike?: never;
+			fillUp?: boolean;
+			cta?: never;
+	  }
+	| {
+			href?: never;
+			buttonLike?: never;
+			fillUp?: never;
+			cta?: (any?: any) => void;
+	  };
+
+type Props = CommonProps & ConditionalProps;
+
 export default function Button({
 	children,
 	styleClass,
 	href,
+	buttonLike = false,
+	fillUp = false,
 	cta,
-}: {
-	children?: React.ReactNode;
-	styleClass?: string;
-	href?: string;
-	cta?: (any?: any) => void;
-}) {
+}: Props) {
 	return href ? (
-		<Link href={href} className={styleClass}>
+		<Link
+			href={href}
+			className={`${styleClass || ''} ${
+				buttonLike
+					? 'rounded-lg border-2 border-red-700 bg-cstm-btn-bg text-cstm-btn-color capitalize hover:shadow hover:shadow-cstm-shadow'
+					: ''
+			} ${fillUp ? 'fill-up' : ''}`}
+		>
 			{children || 'Button'}
 		</Link>
 	) : (
 		<button
 			type='button'
-			className={styleClass}
+			className={`rounded-lg border-1 border-black capitalize hover:shadow hover:shadow-cstm-shadow ${
+				styleClass || ''
+			}`}
 			onClick={() => (cta ? cta() : null)}
 		>
 			{children || 'Button'}
