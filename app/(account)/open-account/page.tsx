41a1d375f6c8a82022e5debc7757a563f8f999b1
Jacek Gizinski <jacektrg+github@gmail.com>
2023-07-16 21:42:53 +0200

    Adding fetching state to render Snipper comp.
    Adding formErrors state. Passing errors to child forms components.
    Rendering Modal with api response data.
    Adding dismissModal function.

--- app/(account)/open-account/page.tsx
+++ app/(account)/open-account/page.tsx
@@ -4,26 +4,33 @@ import Button from '@/app/components/Button';
 import {
 	BusinessAccFormData,
 	RegularAccFormData,
-	VIPAccFormData,
-} from '@/app/lib/types';
+	VipAccFormData,
+} from '@/app/types/types';
 import axios from 'axios';
 
 import { useState } from 'react';
 import Business from './business';
 import Regular from './regular';
-import VIP from './vip';
+import Vip from './vip';
+import { useRouter } from 'next/navigation';
+import Modal from '@/app/components/Modal';
+import Spinner from '@/app/components/Spinner';
 
 export default function OpenAccount() {
 	const [accType, setAccType] = useState<'regular' | 'vip' | 'business' | null>(
 		null
 	);
+	const [formErrors, setFormErrors] = useState<any | null>(null);
+	const [fetching, setFetching] = useState(false);
+	const [accountId, setAccountId] = useState<null | string>(null);
+	const router = useRouter();
 
 	function changeAccountType(type: 'regular' | 'vip' | 'business' | null) {
 		setAccType(type);
 	}
 
 	async function createAccount(
-		data: RegularAccFormData | VIPAccFormData | BusinessAccFormData
+		data: RegularAccFormData | VipAccFormData | BusinessAccFormData
 	) {
 		try {
 			const formErrors = {} as RegularAccFormData;
@@ -33,14 +40,47 @@ export default function OpenAccount() {
 				}
 			}
 			if (Object.keys(formErrors).length !== 0) throw new Error('formErrors');
-			await axios.post(`/api/open-account?accType=${accType}`, data);
-		} catch (error) {
+			setFetching(true);
+			const res = await axios.post(
+				`/api/open-account?accType=${accType}`,
+				data
+			);
+			setAccountId(res.data);
+			setFetching(false);
+		} catch (error: any) {
 			console.log(error);
+			setFormErrors(error);
+			setFetching(false);
 		}
+	}
+
+	function dismissModal(e: React.MouseEvent) {
+		e.stopPropagation();
+		if (accountId) {
+			setAccountId(null);
+			router.push('/login');
+		}
+		if (formErrors) {
+			setFormErrors(null);
+		}
+		setFetching(false);
 	}
 
 	return (
 		<div className='flex flex-col items-center gap-10'>
+			{fetching && <Spinner />}
+			<Modal show={accountId || formErrors} dismiss={dismissModal}>
+				{accountId ? (
+					<>
+						<p>Your account Id:</p>
+						<h3>{accountId}</h3>
+						<p>Make sure you remember it.</p>
+					</>
+				) : formErrors ? (
+					<>render errors !!!</>
+				) : null}
+			</Modal>
+			{/* Fix dropdown anim not working (max-h-screen issue) !!! */}
 			<div className='flex flex-col items-center text-center p-5'>
 				<div
 					className={`overflow-hidden ease-in-out duration-300 ${
@@ -50,6 +90,7 @@ export default function OpenAccount() {
 					<Regular
 						changeAccountType={changeAccountType}
 						createAccount={createAccount}
+						errors={formErrors}
 					/>
 				</div>
 				<div
@@ -57,9 +98,10 @@ export default function OpenAccount() {
 						accType === 'vip' ? 'max-h-screen delay-500' : 'max-h-0'
 					}`}
 				>
-					<VIP
+					<Vip
 						changeAccountType={changeAccountType}
 						createAccount={createAccount}
+						errors={formErrors}
 					/>
 				</div>
 				<div
@@ -70,6 +112,7 @@ export default function OpenAccount() {
 					<Business
 						changeAccountType={changeAccountType}
 						createAccount={createAccount}
+						errors={formErrors}
 					/>
 				</div>
 				<div
@@ -88,10 +131,10 @@ export default function OpenAccount() {
 						</Button>
 					</div>
 					<div className='flex flex-col gap-2'>
-						<h2>VIP Account</h2>
+						<h2>Vip Account</h2>
 						<p>Special account with extra bells and whistles.</p>
 						<Button styleClass='mt-auto' cta={() => changeAccountType('vip')}>
-							Open VIP Account
+							Open Vip Account
 						</Button>
 					</div>
 					<div className='flex flex-col gap-2'>
