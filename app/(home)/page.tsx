8688220ead8e5e26e374011884f85137a5ccc35f
Jacek Gizinski <jacektrg+github@gmail.com>
2023-07-16 21:14:38 +0200

    Redirecting to dashboard if active user session is found.

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,3 +1,18 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { useSession } from 'next-auth/react';
+import Spinner from '../components/Spinner';
+
 export default function Home() {
-	return <div>Home</div>;
+	const { status } = useSession();
+	const router = useRouter();
+
+	useEffect(() => {
+		if (status === 'loading') return;
+		if (status === 'authenticated') router.push('/dashboard');
+	}, [status, router]);
+
+	return status === 'loading' ? <Spinner /> : <div>Home</div>;
 }
