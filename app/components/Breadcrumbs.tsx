8c17855b272575ec0af4b0fb2a1395a98aedf25d
Jacek Gizinski <jacektrg+github@gmail.com>
2023-07-18 19:38:23 +0200

    Minor refactoring. Filtering out empty strings.

--- app/components/Breadcrumbs.tsx
+++ app/components/Breadcrumbs.tsx
@@ -9,22 +9,26 @@ export default function Breadcrumbs() {
 
 	return pathname === '/' ? null : (
 		<div className='flex gap-1 text-sm'>
-			<Link href='/'>Home</Link>
-			{pathname.split('/').map((path, i, arr) => {
-				return pathname === `/${path}` ? (
-					<div key={i}>
-						<div className='underline'>{capitalize(path)}</div>
-						{i !== arr.length - 1 && <span>{'>'}</span>}
-					</div>
-				) : (
-					<div key={i}>
-						<Link key={i} href={path}>
-							{capitalize(path)}
-						</Link>
-						{i !== arr.length - 1 && <span>{'>'}</span>}
-					</div>
-				);
-			})}
+			<Link className='font-normal underline' href='/'>
+				Home
+			</Link>
+			{pathname
+				.split('/')
+				.filter((s) => s)
+				.map((path, i, arr) => {
+					return (
+						<div key={i} className='flex items-center'>
+							<div className='p-1 select-none'>{'>'}</div>
+							{i + 1 === arr.length ? (
+								<div className='py-1 px-2 font-bold'>{capitalize(path)}</div>
+							) : (
+								<Link className='underline' key={i} href={path}>
+									{capitalize(path)}
+								</Link>
+							)}
+						</div>
+					);
+				})}
 		</div>
 	);
 }
