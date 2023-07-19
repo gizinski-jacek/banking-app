e45844c00a150c1997a46d2e13da811f9a8f7fa7
Jacek Gizinski <jacektrg+github@gmail.com>
2023-07-19 22:02:10 +0200

    Updating defaults with new fields

--- app/lib/defaults.ts
+++ app/lib/defaults.ts
@@ -8,14 +8,26 @@ import {
 export const regularAccFormData: RegularAccFormData = {
 	first_name: '',
 	last_name: '',
+	birth_place: '',
+	city: '',
+	address: '',
+	country_calling_code: 0,
+	phone_number: 0,
 	email: '',
+	currency: '',
 	password: '',
 };
 
 export const vipAccFormData: VipAccFormData = {
 	first_name: '',
 	last_name: '',
+	birth_place: '',
+	city: '',
+	address: '',
+	country_calling_code: 0,
+	phone_number: 0,
 	email: '',
+	currency: '',
 	password: '',
 	extra_funds: 0.0,
 };
@@ -23,13 +35,20 @@ export const vipAccFormData: VipAccFormData = {
 export const businessAccFormData: BusinessAccFormData = {
 	first_name: '',
 	last_name: '',
-	company_name: '',
+	birth_place: '',
+	city: '',
 	address: '',
+	country_calling_code: 0,
+	phone_number: 0,
+	company_name: '',
+	company_city: '',
+	company_address: '',
 	email: '',
+	currency: '',
 	password: '',
 };
 
 export const loginData: LoginData = {
-	accountId: '',
+	userId: '',
 	password: '',
 };
