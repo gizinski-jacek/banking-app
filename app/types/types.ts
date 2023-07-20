c76c15e7d1e67f6409fc93e8ab32a99b4f4a2d73
Jacek Gizinski <jacektrg+github@gmail.com>
2023-07-20 23:20:37 +0200

    Adding new fields to account types.
    Cleaning up by merging and reusing.

--- app/types/types.ts
+++ app/types/types.ts
@@ -1,67 +1,110 @@
 import mongoose, { Types } from 'mongoose';
 
-export interface RegularAccFormData {
+export interface BasicAccFormData {
 	first_name: string;
 	last_name: string;
+	birth_date: string;
+	city: string;
+	address: string;
+	country_calling_code: number;
+	phone_number: number;
+	currency: string;
 	email: string;
 	password: string;
 }
 
-export type RegularAccFormErrors = RegularAccFormData;
-
-export interface VipAccFormData extends RegularAccFormData {
+export interface VipAccFormData extends BasicAccFormData {
 	extra_funds: number;
 }
 
-export interface VipAccFormErrors extends Omit<VipAccFormData, 'extra_funds'> {
+export interface BusinessAccFormData extends BasicAccFormData {
+	company_name: string;
+	company_city: string;
+	company_address: string;
+}
+
+export interface BasicAccFormErrors
+	extends Omit<BasicAccFormData, 'country_calling_code' | 'phone_number'> {
+	country_calling_code: string;
+	phone_number: string;
+}
+
+export interface VipAccFormErrors extends BasicAccFormErrors {
 	extra_funds: string;
 }
 
-export interface BusinessAccFormData extends RegularAccFormData {
+export interface BusinessAccFormErrors extends BasicAccFormErrors {
 	company_name: string;
-	address: string;
+	company_city: string;
+	company_address: string;
 }
 
-export type BusinessAccFormErrors = BusinessAccFormData;
-
 export interface LoginData {
-	accountId?: string;
+	userId?: string;
 	password?: string;
 }
 
-export interface NewRegAccountModel extends mongoose.Document {
-	accountId: string;
-	accountType: 'regular';
-	createdAt: number;
-	updatedAt: number;
+export interface UserSchema {
+	userId: string;
 	firstName: string;
 	lastName: string;
+	birthDate: string;
+	city: string;
+	address: string;
+	countryCallingCode: number;
+	phoneNumber: number;
 	email: string;
 	password: string;
+	accounts: Types.ObjectId[];
 }
 
-export interface NewVipAccountModel
-	extends Omit<NewRegAccountModel, 'accountType'> {
-	accountType: 'vip';
-	balance: string;
+export type UserModel = UserSchema &
+	mongoose.Document & {
+		createdAt: number;
+		updatedAt: number;
+	};
+
+export interface AccountSchema {
+	accountType:
+		| 'basic'
+		| 'vip'
+		| 'business'
+		| 'foreignCurrency'
+		| 'savings'
+		| 'junior';
+	companyName?: string;
+	companyCity?: string;
+	companyAddress?: string;
+	currency: string;
+	balance?: number;
 }
 
-export interface NewBusAccountModel
-	extends Omit<NewRegAccountModel, 'accountType'> {
-	accountType: 'business';
-	companyName: string;
-	address: string;
-}
-
-export interface AccountDbDocument extends NewRegAccountModel {
-	_id: Types.ObjectId;
-	balance: string;
-}
+export type AccountModel = AccountSchema &
+	mongoose.Document & {
+		createdAt: number;
+		updatedAt: number;
+	};
 
 export interface SessionUser {
-	accountType: 'regular' | 'vip' | 'business';
-	accountId: string;
+	userId: string;
 	firstName: string;
 	lastName: string;
-	balance: number;
+	accounts: Types.ObjectId[];
 }
+
+export const accountTypes: string[] = [
+	'basic',
+	'vip',
+	'business',
+	'foreignCurrency',
+	'savings',
+	'junior',
+];
+
+export const supportedCurrencies: string[] = [
+	'EUR',
+	'USD',
+	'PLN',
+	'CHF',
+	'GBP',
+];
