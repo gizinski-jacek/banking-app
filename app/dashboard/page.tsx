2d4ac57c8b5c9bae032a0d3e0ecdbe03286a0f2b
Jacek Gizinski <jacektrg+github@gmail.com>
2023-07-31 00:12:17 +0200

    Moving sign out button to layout.
    Moving user session check and redirect to Auth component.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,30 +1,16 @@
 'use client';
 
-import { signOut, useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
-import Spinner from '../components/Spinner';
-import Button from '../components/Button';
+import { useSession } from 'next-auth/react';
 
 export default function Dashboard() {
-	const router = useRouter();
-	const { data: session, status } = useSession({
-		required: true,
-		onUnauthenticated() {
-			router.push('/login');
-		},
-	});
+	const { data: session } = useSession();
 
-	return status === 'loading' ? (
-		<Spinner />
-	) : (
+	return (
 		session && (
 			<div>
 				<h3>Dashboard</h3>
 				<div>{session.user.firstName}</div>
 				<div>{session.user.lastName}</div>
-				<div>{(Math.round(123.123 * 100) / 100).toFixed(2)}</div>
-				<div>{Number(123.123).toFixed(2)}</div>
-				<Button cta={signOut}>Sign Out</Button>
 			</div>
 		)
 	);
