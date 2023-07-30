37097e7b1542e37fcd56ef905d39a8f09839c155
Jacek Gizinski <jacektrg+github@gmail.com>
2023-07-31 00:08:14 +0200

    Updating customAlphabet length to match schema model.
    Throwing new error with generic message in catch block.

--- app/api/open-account/route.ts
+++ app/api/open-account/route.ts
@@ -25,7 +25,7 @@ export async function POST(request: Request) {
 
 		const body = await request.json();
 		await connectDb();
-		const nanoid = customAlphabet('1234567890', 12);
+		const nanoid = customAlphabet('1234567890', 16);
 
 		if (accType === 'basic') {
 			const { errors } = await yupValidation(
@@ -124,6 +124,6 @@ export async function POST(request: Request) {
 		return new Response(null, { status: 200 });
 	} catch (error: any) {
 		console.log(error);
-		return error;
+		throw new Error('Unknown server error.');
 	}
 }
