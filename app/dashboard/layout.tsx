4e83b5cb057a174c7697bf2e56e4a9e9cf5fc7c1
Jacek Gizinski <jacektrg+github@gmail.com>
2023-08-09 21:59:31 +0200

    Adding anchors to additional routes.
    Adding ThemeSelect button.
    Rendering users full name.

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,37 +1,60 @@
 'use client';
 
-import { signOut } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 import Button from '../components/Button';
 import Auth from '../components/Auth';
+import ThemeSelect from '../components/ThemeSelect';
+import { usePathname } from 'next/navigation';
 
 export default function DashboardLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
+	const path = usePathname();
+	const { data: session } = useSession();
+
 	return (
 		<Auth>
-			<nav className='hidden md:block border-purple-600 border-b-2'>
-				<ul className='flex justify-between gap-5 text-xl font-semibold whitespace-nowrap'>
+			<nav className='hidden md:flex justify-between items-center border-purple-600 border-b-2'>
+				<ul className='flex justify-between items-center gap-5 text-xl font-semibold whitespace-nowrap'>
 					<li>
-						<Button href='/dashboard' fillUp={true}>
+						<Button
+							href='/dashboard'
+							active={path === '/dashboard'}
+							fillUp={true}
+						>
 							Dashboard
 						</Button>
 					</li>
 					<li>
-						<Button href='/transfer' fillUp={true}>
+						<Button
+							href='/dashboard/accounts'
+							active={path === '/dashboard/accounts'}
+							fillUp={true}
+						>
+							Accounts
+						</Button>
+					</li>
+					<li>
+						<Button
+							href='/dashboard/transfer'
+							active={path === '/dashboard/transfer'}
+							fillUp={true}
+						>
 							Transfer
 						</Button>
 					</li>
-					<li>
-						<Button href='/transfer' fillUp={true}>
-							Account
-						</Button>
-					</li>
-					<li>
-						<Button cta={signOut}>Sign Out</Button>
-					</li>
 				</ul>
+				<div className='flex items-center gap-2'>
+					<h3>
+						{session?.user.firstName} {session?.user.lastName}
+					</h3>
+					<ThemeSelect />
+					<Button styleClass='p-0 px-1' cta={signOut}>
+						Sign Out
+					</Button>
+				</div>
 			</nav>
 			{children}
 		</Auth>
