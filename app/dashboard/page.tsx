1a8533b4dcc4033eab8bed7e5f33172cd016420e
Jacek Gizinski <jacektrg+github@gmail.com>
2023-08-09 22:01:13 +0200

    Rendering user's account as flexbos items.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,28 @@
 'use client';
 
 import { useSession } from 'next-auth/react';
+import capitalize from '../lib/capitalize';
 
 export default function Dashboard() {
 	const { data: session } = useSession();
 
 	return (
 		session && (
-			<div>
-				<h3>Dashboard</h3>
-				<div>{session.user.firstName}</div>
-				<div>{session.user.lastName}</div>
+			<div className='flex'>
+				{session.user.accounts.map((acc) => {
+					return (
+						<div
+							key={acc._id}
+							className='p-2 rounded-md border-2 border-cstm-shadow flex flex-col gap-2'
+						>
+							<h3>{capitalize(acc.accountType)}</h3>
+							<div>
+								{(Math.round(acc.balance * 100) / 100).toFixed(2)}{' '}
+								{acc.currency}
+							</div>
+						</div>
+					);
+				})}
 			</div>
 		)
 	);
